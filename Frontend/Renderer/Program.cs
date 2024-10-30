fb10f8f867dcb266523896198e9c8aa10ee63b68
Johan Samper <johansamperg@gmail.com>
2024-10-30 17:29:34 -0500

    Ajustes para inyectar blazor

--- Frontend/Renderer/Program.cs
+++ Frontend/Renderer/Program.cs
@@ -10,6 +10,8 @@ builder.Services.AddViewComponentModels();
 builder.Services.AddFormViewComponentModels();
 builder.Services.AddScoped<NavbarWidgetViewModel>();
 builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
 
 var app = builder.Build();
 
@@ -27,10 +29,21 @@ else
 app.UseStaticFiles();
 app.UseRouting();
 app.UseSitefinity();
+app.MapBlazorHub();
 
 app.UseEndpoints(endpoints =>
 {
     endpoints.MapSitefinityEndpoints();
 });
+
+if (app.Environment.IsDevelopment())
+{
+    app.Use(async (context, next) =>
+    {
+        context.Response.Headers.Add("Content-Security-Policy",
+            "default-src 'self'; connect-src 'self' wss://localhost:44329 https://*.insight.sitefinity.com https://*.dec.sitefinity.com wss://localhost;");
+        await next();
+    });
+}
 
 app.Run();
---
+++ Frontend/Renderer/ViewComponents/Counter/CounterViewComponent.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Progress.Sitefinity.AspNetCore.ViewComponents;
+
+namespace Renderer.ViewComponents.Counter
+{
+    [SitefinityWidget]
+    public class CounterViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View("Counter");
+        }
+    }
+}
